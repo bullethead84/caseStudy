705ee47cb4f80f713357f72eeeb34859ceac27cd
bullethead <daniel.eysert@aconso.com>
2024-01-26 13:37:01 +0100

    :bug: correct init.sql

--- initdb.d/init.sql
+++ initdb.d/init.sql
@@ -1,11 +1,12 @@
 CREATE
-DATABASE aconso
-WITH OWNER = postgres
+    DATABASE aconso
+    WITH OWNER = postgres
     ENCODING = 'UTF8'
     CONNECTION LIMIT = -1;
 
 CREATE USER aconso WITH PASSWORD 'secret';
 GRANT CONNECT ON DATABASE aconso TO aconso;
-GRANT ALL ON schema public TO aconso;
-
 GRANT ALL PRIVILEGES ON DATABASE aconso TO aconso;
+\c aconso postgres
+GRANT ALL PRIVILEGES ON SCHEMA public TO aconso;
+GRANT ALL ON SCHEMA public TO aconso;
